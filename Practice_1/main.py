80d9049be838161610438f9b3c3e3c43546ccbfa
Ilia <illigiorno@gmail.com>
2024-03-25 17:34:17 +0400

    Initial commit

---
+++ Practice_1/main.py
@@ -0,0 +1,93 @@
+import random
+import time
+
+
+def bubble_sort(mas):
+    for j in range(1, len(mas) - 1):
+        for i in range(0, len(mas) - 1):
+            if mas[i] > mas[i + 1]:
+                v = mas[i]
+                mas[i] = mas[i + 1]
+                mas[i + 1] = v
+    return mas
+
+
+def quick_sort(mas):
+    if len(mas) <= 1:
+        return mas
+    else:
+        index_of_support_element = random.randint(0, len(mas) - 1)
+        min_mas = []
+        equal_mas = []
+        max_mas = []
+        for i in range(0, len(mas)):
+            if mas[i] < mas[index_of_support_element]:
+                min_mas.append(mas[i])
+            elif mas[i] == mas[index_of_support_element]:
+                equal_mas.append(mas[i])
+            elif mas[i] > mas[index_of_support_element]:
+                max_mas.append((mas[i]))
+        return quick_sort(min_mas) + equal_mas + quick_sort(max_mas)
+
+
+def function(count):
+    # count = random.randint(1000, 10000)
+    print("Количество элементов в массиве: " + str(count))
+    arr1 = []
+    for i in range(0, count):
+        arr1.append(random.randint(-1000, 1000))
+
+    arr2 = arr1.copy()
+
+    start_time = time.time()
+    bubble_sort(arr1)
+    tmp_time = time.time() - start_time
+    print("Время выполнения сортировки пузырьком: ")
+    print(tmp_time)
+
+    start_2_time = time.time()
+    arr2 = quick_sort(arr2)
+    tmp_time = time.time() - start_2_time
+    print("Время выполнения быстрой сортировки: ")
+    print(tmp_time)
+
+
+def check_function():
+    count = random.randint(10, 20)
+    print(count)
+    arr1 = []
+    for i in range(0, count):
+        arr1.append(random.randint(-1000, 1000))
+
+    arr2 = arr1.copy()
+
+    print("Массив до сортировки пузырьком: ")
+    print(arr1)
+    start_time = time.time()
+    bubble_sort(arr1)
+    print("Время выполнения сортировки пузырьком: ")
+    print(time.time() - start_time)
+    print("Массив после сортировки пузырьком: ")
+    print(arr1)
+
+    print("\n")
+
+    print("Массив до быстрой сортировки: ")
+    print(arr2)
+    start_2_time = time.time()
+    arr2 = quick_sort(arr2)
+    print(arr2)
+    print("Время выполнения быстрой сортировки: ")
+    print(time.time() - start_2_time)
+    print("Массив после быстрой сортировки: ")
+    print(arr2)
+
+
+if __name__ == "__main__":
+    print("Практическая работа № 1. Алгоритмы сортировки, Вариант 5. Выполнил: Лысов Илья, 6204-020302D")
+    # function(1000)
+    # print("\n")
+    function(5000)
+    print("\n")
+    # function(10000)
+    # print("\n")
